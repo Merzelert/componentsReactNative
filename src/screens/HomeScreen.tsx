7102a316c833b0fd144ebcbd60c6ca1cf08dbf52
Erik Ivan Retana Lopez <merzelert@gmail.com>
2023-03-14 09:36:15 -0600

    final home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import Icon from 'react-native-vector-icons/Ionicons';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { ItemSeparator } from '../components/ItemSeparator';
 import { menuItems } from '../data/menuItems';
 import { styles } from '../theme/appTheme';
+import { ThemeContext } from '../context/themeContext/ThemeContext';
 
 export const HomeScreen = () => {
+
+    const {theme} = useContext(ThemeContext);
 
     return (
         <View style={{ flex: 1, ...styles.globalMargin }}>
@@ -21,6 +24,9 @@ export const HomeScreen = () => {
                 showsVerticalScrollIndicator={false} // Aquí se le pasa un booleano que indica si se mostrará o no el scroll vertical
                 ItemSeparatorComponent={ItemSeparator} // Aquí se le pasa la función que se encargará de renderizar el separador de cada item de la lista
             />
+            <View>
+                    <Text style={{textAlign:'center', fontSize:20, fontWeight:'bold', marginBottom:80, alignItems: 'flex-end', color: theme.colors.primary}}>Erik Retana Dev</Text>
+            </View>
         </View>
     );
 };
